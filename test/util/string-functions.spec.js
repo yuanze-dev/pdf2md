3c7bd96160f42e140b481a869d3ed6be97a6ab73
Jenssen Lee <30688276+helplah@users.noreply.github.com>
2019-10-03 18:54:59 +0800

    Add findFirstPage that check and return first page

--- test/util/string-functions.spec.js
+++ test/util/string-functions.spec.js
@@ -1,7 +1,7 @@
 const { expect } = require('chai')
 
 const { hasUpperCaseCharacterInMiddleOfWord, normalizedCharCodeArray, removeLeadingWhitespaces, removeTrailingWhitespaces, prefixAfterWhitespace, suffixBeforeWhitespace, charCodeArray, isListItem, isNumberedListItem, wordMatch } = require('../../lib/util/string-functions')
-const { findPageNumber } = require('../../lib/util/pdf')
+const { findPageNumbers, findFirstPage } = require('../../lib/util/pdf')
 
 describe('functions: hasUpperCaseCharacterInMiddleOfWord', () => {
   it('single word', () => {
@@ -180,9 +180,43 @@ describe('functions: wordsMatch', () => {
   })
 })
 
-describe.only('functions: findPageNumber', () => {
-  it('Searches and stores page number', () => {
+describe('functions: findPageNumbers', () => {
+  it('Search, coerce and store page number', () => {
     const array = [{ str: '1' }, { str: 'test' }, { str: 'how' }, { str: 'to' }, { str: 'find' }, { str: 'page' }, { str: '3' }, { str: 'number' }]
-    expect(findPageNumber({}, '3', array)).to.eql({ 3: ['1', '3'] })
+    expect(findPageNumbers({}, 3, array)).to.eql({ 3: [1, 3] })
+  })
+})
+
+describe.only('functions: findFirstPage', () => {
+  it('Return first page index and number with completed Hashtable', () => {
+    const hashTable = {
+      20: [3],
+      21: [4],
+      22: [5],
+      23: [6],
+      24: [7],
+      25: [8],
+      26: [9],
+      27: [10],
+      28: [11],
+      30: [13],
+    }
+    expect(findFirstPage(hashTable)).to.eql(['20', [3]])
+  })
+
+  it('Return first page index and number with incompleted Hashtable', () => {
+    const hashTable = {
+      2: [19, 86, 1986, 110],
+      5: [137, 151],
+      9: [1],
+      10: [4],
+      12: [6],
+      13: [8, 7],
+      14: [8],
+      15: [10, 9],
+      16: [11, 12, 10],
+      17: [11],
+    }
+    expect(findFirstPage(hashTable)).to.eql(['10', [4]])
   })
 })
